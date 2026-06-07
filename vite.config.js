b60b40b5715d7ba7ff2f1b2b715f3c2c16bf3cc4
vcbot88 <vinicius.cbot@gmail.com>
2026-06-07 17:38:38 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',
   build: {
     outDir: 'dist',
-    sourcemap: false,
+    rollupOptions: {
+      input: 'index.html'
+    }
   }
 })
